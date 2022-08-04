6016437f31c81d6be8076209e0b37fb15c445865
hhkwairanga <harunahk5575@gmail.com>
2022-08-04 21:52:13 +0100

    first

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+* sum_them_all - return sum when when given numbers
+* @n: int to be added 
+* Return: sum if not 0
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list constantSum;
+	unsigned int sum = 0;
+	unsigned int j;
+	
+	if (n == 0)
+		return (0);
+	va_start(constantSum, n);
+
+	for (j = 0; j < n; j++)
+		sum += va_arg(constantSum, int);	va_end(constantSum); 
+
+	return (sum);
+}
+
