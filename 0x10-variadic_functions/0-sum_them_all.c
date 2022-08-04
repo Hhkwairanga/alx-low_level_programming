a2072a0d2ab24e2034ccfa2a57f172d5fac8463d
hhkwairanga <harunahk5575@gmail.com>
2022-08-04 22:23:09 +0100

    betty comment

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,26 +2,25 @@
 #include "variadic_functions.h"
 
 /**
-* sum_them_all - return sum when when given numbers
-* @n: int to be added 
-* Return: sum if not 0
-*/
+ * sum_them_all - return sum when given unknown amount of arguments
+ * @n: integers to add
+ * Return: sum
+ */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list constantSum;
-	unsigned int sum = 0;
-	unsigned int j;
-/* validate valist and initialize */	
+	va_list valist;
+	unsigned int sum = 0, i;
+
+	/* validate valist and initialize */
 	if (n == 0)
 		return (0);
-	va_start(constantSum, n);
+	va_start(valist, n);
 
-	/* iterate through list, update sum, fre    e list */	
-	for (j = 0; j < n; j++)
-		sum += va_arg(constantSum, int);
-	va_end(constantSum); 
+	/* iterate through list, update sum, free list */
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+	va_end(valist);
 
 	return (sum);
 }
-
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,21 +1,25 @@
-#include <stdarg.h>
-  2 #include "variadic_functions.h"
-  3
+#include "variadic_functions.h"
+#include <stdio.h>
+
 /**
-* sum_them_all - return sum when given unknown     amount of arguments
-* @n: integers to add
-* Return: sum
-*/
-  void print_numbers(const char *separator, const unsigned int n, ...);
- 11 {
- 12         va_list valist;
- 13         unsigned int sum = 0, i;
- 16         if (n == 0)
- 17                 return (0);
- 18         va_start(valist, n);
- 19
- 22                 sum += va_arg(valist, int);
- 23         va_end(valist);
- 24
- 25         return (sum);
- 26 }
+ * print_numbers - print each number with separator, followed by a newline
+ * @separator: string to be printed between numbers
+ * @n: number of args passed
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, int));
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(list);
+}
